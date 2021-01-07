389bdf1577a9475a5d734439eec0d7764a777be6
stevecorya <steve@stevecorya.com>
2021-01-07 01:55:20 -0500

    consistent spacing

--- logger.js
+++ logger.js
@@ -1,9 +1,9 @@
 "use strict";
 
 const DateFormat = require ('fast-date-format');
-const dateFormat = new DateFormat('YYYY[-]MM[-]DD HH[:]mm[:]ss');
+const dateFormat = new DateFormat ('YYYY[-]MM[-]DD HH[:]mm[:]ss');
 
-module.exports = require('console-log-level') ({
+module.exports = require ('console-log-level') ({
     prefix: function (level) {
         return `[agassi] ${dateFormat.format (new Date ())} [${level}]`
     },
