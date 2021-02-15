a9de0b44d4f660cf86aa6ae35a4847ddfacea3b7
Steve Corya <steve@stevecorya.com>
2021-02-14 22:08:21 -0500

    move everything to main function

--- index.js
+++ index.js
@@ -58,96 +58,96 @@ function main () {
         rqlite.initialize (address);
         Cluster.start (address, subnet, Config.standalone);
     });
-}
 
-// wait for rqlited to start
-rqlited.status.once ('ready', async () => {
-    Cluster.advertise ('ready');
-    if (rqlited.isLeader ()) {
-        // initialize ACME accont and database tables
-        await ACME.createAccount ();
-        log.debug ('Initializing rqlite tables...');
-        const tableCreationTransaction = await rqlite.dbTransact ([
-            Query.services.createTable,
-            Query.challenges.createTable,
-            Query.certificates.createTable
-        ]);
-        log.debug (`Initialized tables in ${tableCreationTransaction.time * 1000} ms.`);
-    }
-    // start listening to Docker socket
-    Docker.Events.start ();
-});
-
-// add possible existing services on socket connection
-Docker.Events.on ('connect' , async function checkExistingServices () {
-    if (rqlited.isLeader ()) {
-        log.debug ('Checking existing docker services for agassi labels...');
-        // get all service ID's
-        const allSwarmServiceIDs = (await Docker.API.listServices ()).map (service => service.ID);
-
-        // filter those which have the requisite labels
-        const agassiSwarmServices = [];
-        for (let id of allSwarmServiceIDs) {
-            const service = await Docker.API.getService (id).inspect ();
-            if (Docker.isAgassiService (service)) {
-                log.debug (`Found agassi service ${service.ID}.`);
-                agassiSwarmServices.push (service);
-            }
+    // wait for rqlited to start
+    rqlited.status.once ('ready', async () => {
+        Cluster.advertise ('ready');
+        if (rqlited.isLeader ()) {
+            // initialize ACME accont and database tables
+            await ACME.createAccount ();
+            log.debug ('Initializing rqlite tables...');
+            const tableCreationTransaction = await rqlite.dbTransact ([
+                Query.services.createTable,
+                Query.challenges.createTable,
+                Query.certificates.createTable
+            ]);
+            log.debug (`Initialized tables in ${tableCreationTransaction.time * 1000} ms.`);
         }
+        // start listening to Docker socket
+        Docker.Events.start ();
+    });
 
-        // pull rqlited services from database
-        const dbServiceIDs = (await rqlite.dbQuery ('SELECT id FROM services;', 'strong')).results.map (result => result.id);
+    // add possible existing services on socket connection
+    Docker.Events.on ('connect' , async function checkExistingServices () {
+        if (rqlited.isLeader ()) {
+            log.debug ('Checking existing docker services for agassi labels...');
+            // get all service ID's
+            const allSwarmServiceIDs = (await Docker.API.listServices ()).map (service => service.ID);
 
-        log.debug (`Database has (${dbServiceIDs.length}/${agassiSwarmServices.length}) docker services.`);
+            // filter those which have the requisite labels
+            const agassiSwarmServices = [];
+            for (let id of allSwarmServiceIDs) {
+                const service = await Docker.API.getService (id).inspect ();
+                if (Docker.isAgassiService (service)) {
+                    log.debug (`Found agassi service ${service.ID}.`);
+                    agassiSwarmServices.push (service);
+                }
+            }
 
-        // if swarm has service that rqlited doesn't, add service and cert to rqlited
-        agassiSwarmServices.filter (service => !dbServiceIDs.includes (service.ID)).forEach (async (service) => {
-            await Docker.pushServiceToDB (service);
-            await ACME.certify (Docker.parseServiceLabels(service)[Config.serviceLabelPrefix + 'domain']);
-        });
+            // pull rqlited services from database
+            const dbServiceIDs = (await rqlite.dbQuery ('SELECT id FROM services;', 'strong')).results.map (result => result.id);
 
-        // if rqlited has service that swarm doesn't, remvoe the service and not the cert
-        dbServiceIDs.filter (id => !allSwarmServiceIDs.includes (id)).forEach (async (id) => {
-            await Docker.removeServiceFromDB (id);
-        });
-    }
-    HTTPS.start ();
-});
+            log.debug (`Database has (${dbServiceIDs.length}/${agassiSwarmServices.length}) docker services.`);
 
-HTTPS.Server.once ('listening', () => {
-    ACME.Maintenance.start ();
-});
+            // if swarm has service that rqlited doesn't, add service and cert to rqlited
+            agassiSwarmServices.filter (service => !dbServiceIDs.includes (service.ID)).forEach (async (service) => {
+                await Docker.pushServiceToDB (service);
+                await ACME.certify (Docker.parseServiceLabels(service)[Config.serviceLabelPrefix + 'domain']);
+            });
 
-rqlited.status.on ('disconnected', () => {
-    HTTPS.stop ();
-    Cluster.advertise ('disconnected');
-});
+            // if rqlited has service that swarm doesn't, remvoe the service and not the cert
+            dbServiceIDs.filter (id => !allSwarmServiceIDs.includes (id)).forEach (async (id) => {
+                await Docker.removeServiceFromDB (id);
+            });
+        }
+        HTTPS.start ();
+    });
 
-rqlited.status.on ('reconnected', () => {
-    HTTPS.start ();
-    Cluster.advertise ('reconnected');
-});
+    HTTPS.Server.once ('listening', () => {
+        ACME.Maintenance.start ();
+    });
 
-Docker.Events.on ('_message', async function processDockerEvent (event) {
-    if (rqlited.isLeader ()) {
-        // on service creation, update or removal
-        if (event.Type === 'service') {
-            const service = await Docker.API.getService (event.Actor.ID).inspect ();
-            if (Docker.isAgassiService (service)) {
+    rqlited.status.on ('disconnected', () => {
+        HTTPS.stop ();
+        Cluster.advertise ('disconnected');
+    });
 
-                if (event.Action === 'update' || event.Action === 'create') {
-                    await Docker.pushServiceToDB (service);
-                    if (event.Action === 'create') {
-                        await ACME.certify (Docker.parseServiceLabels(service)[Config.serviceLabelPrefix + 'domain']);
+    rqlited.status.on ('reconnected', () => {
+        HTTPS.start ();
+        Cluster.advertise ('reconnected');
+    });
+
+    Docker.Events.on ('_message', async function processDockerEvent (event) {
+        if (rqlited.isLeader ()) {
+            // on service creation, update or removal
+            if (event.Type === 'service') {
+                const service = await Docker.API.getService (event.Actor.ID).inspect ();
+                if (Docker.isAgassiService (service)) {
+
+                    if (event.Action === 'update' || event.Action === 'create') {
+                        await Docker.pushServiceToDB (service);
+                        if (event.Action === 'create') {
+                            await ACME.certify (Docker.parseServiceLabels(service)[Config.serviceLabelPrefix + 'domain']);
+                        }
+                    }
+                    if (event.Action === 'remove') {
+                        await Docker.removeServiceFromDB (event.Actor.ID);
                     }
                 }
-                if (event.Action === 'remove') {
-                    await Docker.removeServiceFromDB (event.Actor.ID);
-                }
             }
         }
-    }
-});
+    });
+}
 
 process.on ('SIGINT', () => {
     log.info ('SIGINT ignored, use SIGTERM to exit.');
