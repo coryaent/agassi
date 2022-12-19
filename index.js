6e7a4a754c045533f7a959098a4907ff1de6e06c
Stephen Corya <steve@corya.net>
2022-12-19 14:40:36 -0500

    add dockerode

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
 "use strict";
 
 
-const {createSafeRedisLeader} = require('safe-redis-leader')
 const Redis = require('ioredis')
 const uuid = require('uuid');
 
 const redis = new Redis({
     host: process.env.AGASSI_REDIS_HOST,
-    port: 6379
-})
-redis.set("mykey", "value"); // Returns a promise which resolves to "OK" when the command succeeds.
-
-// ioredis supports the node.js callback style
-redis.get("mykey", (err, result) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(result); // Prints "value"
-  }
-});
-
-// Or ioredis returns a promise if the last argument isn't a function
-redis.get("mykey").then((result) => {
-  console.log(result); // Prints "value"
+    port: process.env.AGASSI_REDIS_PORT
 });
