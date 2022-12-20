63fb03fe0e73f2d5afac2848a8583f80204f36b9
Stephen Corya <steve@corya.net>
2022-12-20 12:07:35 -0500

    handle service labels

--- index.js
+++ index.js
@@ -26,22 +26,99 @@ const docker = new Docker ({
     version: 'v1.37'
 });
 
+const optRegEx = /opt(?:(?:ion)?s|ion)?/i;
+
 // if client start monitoring docker socket
 if (process.argv.includes ('--client')) {
-    const events = await docker.getEvents ({ filters: { type: ["service"]}});
-    events.on ('data', async (data) => {
-        let event = JSON.parse (data);
-        uconsole.log (event);
-        if (event.Action == 'create' || event.Action == 'update') {
-            let service = await docker.getService (event.Actor.ID);
-            console.log (await service.inspect ());
-        }
-        if (event.Action == 'remove') {
-        }
+    docker.getEvents ({ filters: { type: ["service"]}}).then (events => {
+        events.on ('data', async (data) => {
+            let event = JSON.parse (data);
+            console.log (event);
+            if (event.Action == 'create' || event.Action == 'update') {
+                let service = await docker.getService (event.Actor.ID);
+                service = await service.inspect ();
+                console.log (service);
+                console.log (parseServiceLabels (service));
+                console.log (parseProxyOptions (parseServiceLabels (service)));
+            }
+            if (event.Action == 'remove') {
+            }
+        });
     });
 }
 
 // if server start HTTPS server
 if (process.argv.includes ('--server')) {
 
+}
+
+function parseServiceLabels (service) {
+    // merge service labels, prefering container labels to service labels
+    const labels = {};
+    const labelsMap = new Map ();
+
+    //  service:
+    //    image:
+    //    deploy:
+    //      labels:
+    if (service.Spec.Labels) {
+        const serviceLabels = service.Spec.Labels;
+        Object.keys (serviceLabels).forEach ((labelKey) => {
+            labelsMap.set (labelKey, serviceLabels[labelKey]);
+        });
+    }
+
+    //  service:
+    //    image:
+    //    labels:
+    if (service.Spec.TaskTemplate.ContainerSpec.Labels) {
+        const containerLabels = service.Spec.TaskTemplate.ContainerSpec.Labels;
+        Object.keys (containerLabels).forEach ((labelKey) => {
+            labelsMap.set (labelKey, containerLabels[labelKey]);
+        });
+    }
+
+    labelsMap.forEach ((value, key) => {
+        labels[key] = value;
+    });
+
+    return labels;
+}
+
+function parseProxyOptions (labels) {
+    // http-proxy options
+    const options = {};
+
+    Object.keys (labels).forEach ((labelKey) => {
+        // filter labels that start with the prefix
+        if (labelKey.startsWith ('site.agassi.')) {
+            const agassiLabel = labelKey.replace ('site.agassi.', '');
+            // filter labels that define a proxy option
+            if (optRegEx.test (agassiLabel)) {
+                const optionKey = camelCase (agassiLabel.substring (agassiLabel.lastIndexOf ('.') + 1));
+                // set the proxy options
+                switch (optionKey) {
+                    case 'agent':
+                    case 'ssl':
+                    case 'ws':
+                    case 'prependPath':
+                    case 'ignorePath':
+                    case 'selfHandleResponse':
+                    case 'buffer':
+                        log.warn (`Label ${labelKey} ignored.`);
+                        break;
+                    default:
+                        options[optionKey] = labels[labelKey];
+                }
+            }
+        }
+    });
+
+    return options;
+}
+
+function camelCase (text) {
+    return text.replace (/(?:[_-])(\w)/g, function (_, c) {
+        return c ? c.toUpperCase () : '';
+    }); 
 }
