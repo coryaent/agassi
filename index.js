18f17cc4061665566b5e4ce428fc31ce0a7f563d
Stephen Corya <steve@corya.net>
2022-12-20 09:27:58 -0500

    client/server logic

--- index.js
+++ index.js
@@ -4,6 +4,17 @@
 const Redis = require('ioredis')
 const Docker = require ('dockerode');
 
+// check argv
+if (!process.argv.includes ('--client') && !process.argv.includes ('--server')) {
+    console.error ('must specify client or server mode');
+    process.exit (1);
+}
+if (process.argv.includes ('--client') && process.argv.includes ('--server')) {
+    console.error ('cannot run as client and server simultaneously');
+    process.exit (1);
+}
+
+// initialization
 const redis = new Redis({
     host: process.env.AGASSI_REDIS_HOST,
     port: process.env.AGASSI_REDIS_PORT
@@ -15,14 +26,22 @@ const docker = new Docker ({
     version: 'v1.37'
 });
 
-(async () => {
-
-    // watch services
+// if client start monitoring docker socket
+if (process.argv.includes ('--client')) {
     const events = await docker.getEvents ({ filters: { type: ["service"]}});
     events.on ('data', async (data) => {
         let event = JSON.parse (data);
-        let service = await docker.getService (event.Actor.ID);
-        console.log (await service.inspect ());
+        uconsole.log (event);
+        if (event.Action == 'create' || event.Action == 'update') {
+            let service = await docker.getService (event.Actor.ID);
+            console.log (await service.inspect ());
+        }
+        if (event.Action == 'remove') {
+        }
     });
+}
 
-}) ();
+// if server start HTTPS server
+if (process.argv.includes ('--server')) {
+
+}
