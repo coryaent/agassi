058586fffc7126dcda604d36497af17fa7c03899
Stephen Corya <steve@corya.net>
2022-12-25 13:28:19 -0500

    switch to production let's encrypt

--- fetchCertificate.js
+++ fetchCertificate.js
@@ -13,7 +13,6 @@ const accountKeys = forge.pki.rsa.generateKeyPair (4096);
 const accountPrivateKey = forge.pki.privateKeyToPem (accountKeys.privateKey);
 
 const client = new acme.Client({
-    directoryUrl: acme.directory.letsencrypt.staging,
     accountKey: accountPrivateKey
 });
 
