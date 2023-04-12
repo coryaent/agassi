427745f04fef5cee607350cb6f97c70ccd755ff0
Stephen Corya <steve@corya.net>
2023-04-12 15:49:12 -0400

    remove api token

--- cpanel.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const log = require ('./logger.js');
-const axios = require ('axios');
-
-(async () => {
-    log.debug ((await axios.get ('https://fiber12.dnsiaas.com:2083/execute/DNS/parse_zone?zone=corya.net', {headers: {'Authorization': 'cpanel coryane1:DKYVPFOCN06F9U82KCEYJWTU4SJKLVSM'}})).data.data);
-}) ();
