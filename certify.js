df93fdfa3070a2c8c237b8f77fff0d0624aed7e3
Stephen Corya <steve@corya.net>
2023-04-26 13:22:44 -0400

    crash on certification error

--- certify.js
+++ certify.js
@@ -29,76 +29,72 @@ const redis = new Redis ({
 });
 
 module.exports = async function (domain) {
-    try {
-        const account = await acmeClient.createAccount({
-            termsOfServiceAgreed: true,
-            contact: [`mailto:${process.env.AGASSI_LETS_ENCRYPT_EMAIL}`]
-        });
-        log.debug ('creating certificate order')
-        const order = await acmeClient.createOrder({
-            identifiers: [
-                { type: 'dns', value: domain },
-            ]
-        });
+    const account = await acmeClient.createAccount({
+        termsOfServiceAgreed: true,
+        contact: [`mailto:${process.env.AGASSI_LETS_ENCRYPT_EMAIL}`]
+    });
+    log.debug ('creating certificate order')
+    const order = await acmeClient.createOrder({
+        identifiers: [
+            { type: 'dns', value: domain },
+        ]
+    });
 
-        log.debug ('fetching authorizations');
-        const authorizations = await acmeClient.getAuthorizations (order);
-        log.debug ('finding dns challenge');
-        const dnsChallenge = authorizations[0]['challenges'].find ((element) => element.type === 'dns-01');
+    log.debug ('fetching authorizations');
+    const authorizations = await acmeClient.getAuthorizations (order);
+    log.debug ('finding dns challenge');
+    const dnsChallenge = authorizations[0]['challenges'].find ((element) => element.type === 'dns-01');
 
-        log.debug ('fetching key authorization');
-        const keyAuthorization = await acmeClient.getChallengeKeyAuthorization(dnsChallenge);
+    log.debug ('fetching key authorization');
+    const keyAuthorization = await acmeClient.getChallengeKeyAuthorization(dnsChallenge);
 
-        // set txt (ACME)
-        log.debug ('setting txt record');
-        log.trace (`${domain} -> ${keyAuthorization}`);
-        const txtSet = await putTxtRecord (`_acme-challenge.${domain}`, keyAuthorization);
-        log.trace (txtSet.data);
+    // set txt (ACME)
+    log.debug ('setting txt record');
+    log.trace (`${domain} -> ${keyAuthorization}`);
+    const txtSet = await putTxtRecord (`_acme-challenge.${domain}`, keyAuthorization);
+    log.trace (txtSet.data);
 
-        // complete challenge
-        log.debug ('completing challenge');
-        const completion = await acmeClient.completeChallenge (dnsChallenge);
+    // complete challenge
+    log.debug ('completing challenge');
+    const completion = await acmeClient.completeChallenge (dnsChallenge);
 
-        // await validation
-        log.debug ('awaiting validation...');
-        // await acmeClient.waitForValidStatus (dnsChallenge)
-        // let validation = await retry (async function (retry, number) {
-        //     log.info ('attempt number', number);
-        //     return acmeClient.waitForValidStatus (dnsChallenge).catch (retry);
-        // });
-        await sleep (7500);
-        let validation = await acmeClient.waitForValidStatus (dnsChallenge)
-        //  let validation = await awaitValidStatus (dnsChallenge);
+    // await validation
+    log.debug ('awaiting validation...');
+    // await acmeClient.waitForValidStatus (dnsChallenge)
+    // let validation = await retry (async function (retry, number) {
+    //     log.info ('attempt number', number);
+    //     return acmeClient.waitForValidStatus (dnsChallenge).catch (retry);
+    // });
+    await sleep (7500);
+    let validation = await acmeClient.waitForValidStatus (dnsChallenge)
+    //  let validation = await awaitValidStatus (dnsChallenge);
 
-        log.debug ('creating csr');
-        const [key, csr] = await acme.crypto.createCsr ({
-            commonName: domain
-        }, fs.readFileSync (process.env.AGASSI_DEFAULT_KEY_FILE));
+    log.debug ('creating csr');
+    const [key, csr] = await acme.crypto.createCsr ({
+        commonName: domain
+    }, fs.readFileSync (process.env.AGASSI_DEFAULT_KEY_FILE));
 
-        log.debug ('finalizing order')
-        const finalized = await acmeClient.finalizeOrder (order, csr);
+    log.debug ('finalizing order')
+    const finalized = await acmeClient.finalizeOrder (order, csr);
 
-        log.debug ('fetching cert');
-        let cert = await acmeClient.getCertificate (finalized);
-        // I do not know why this is necessary, but getCertificate seems to return three of the same cert in one file.
-        cert = cert.substring (0, cert.indexOf ('-----END CERTIFICATE-----')).concat ('-----END CERTIFICATE-----');
+    log.debug ('fetching cert');
+    let cert = await acmeClient.getCertificate (finalized);
+    // I do not know why this is necessary, but getCertificate seems to return three of the same cert in one file.
+    cert = cert.substring (0, cert.indexOf ('-----END CERTIFICATE-----')).concat ('-----END CERTIFICATE-----');
 
-        log.debug ('reading expiration');
-        const { validTo } = new X509Certificate (cert);
-        const expiration = new Date (validTo);
+    log.debug ('reading expiration');
+    const { validTo } = new X509Certificate (cert);
+    const expiration = new Date (validTo);
 
-        // remove challenge
-        // log.debug ('removing challenge key');
-        // const txtDelete = await deleteTxtRecord (`_acme-challenge.${domain}`);
+    // remove challenge
+    // log.debug ('removing challenge key');
+    // const txtDelete = await deleteTxtRecord (`_acme-challenge.${domain}`);
 
-        log.debug ('expiration ' + expiration);
-        log.debug ('adding cert to redis');
-        let res = await redis.set (`cert${process.env.AGASSI_ACME_PRODUCTION ? '' : '.staging'}:${domain}`, cert,
-                               'PX', new Date (expiration).getTime () - new Date ().getTime ());
-        log.debug (res);
-    } catch (error) {
-        log.error (error.message);
-    }
+    log.debug ('expiration ' + expiration);
+    log.debug ('adding cert to redis');
+    let res = await redis.set (`cert${process.env.AGASSI_ACME_PRODUCTION ? '' : '.staging'}:${domain}`, cert,
+                            'PX', new Date (expiration).getTime () - new Date ().getTime ());
+    log.debug (res);
 }
 
 // const awaitValidStatus = async (dnsChallenge) =>
