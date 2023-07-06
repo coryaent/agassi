33148f991b7664a796ebb0a53031ec30f95cd279
Stephen Corya <stephen@corya.net>
2023-07-05 20:49:44 -0500

    switch to etcd

--- server.js
+++ server.js
@@ -10,46 +10,98 @@ const rateLimit = require ('http-ratelimit');
 const memoize = require ('nano-memoize');
 const bcrypt = require ('bcryptjs');
 const compare = require ('tsscmp');
-const generateCertificate = require ('./generateCertificate.js');
-const Redis = require ('ioredis')
-
+const { Etcd3 } = require ('etcd3');
 const Proxy = require ('./proxy.js');
 
 // initializations
-
-const redis = new Redis ({
-    host: process.env.AGASSI_REDIS_HOST,
-    port: process.env.AGASSI_REDIS_PORT
+const etcdClient = new Etcd3({
+    hosts: process.env.AGASSI_ETCD_HOSTS.split (',')
 });
 const compareHash = memoize (bcrypt.compare, {maxAge: 1000 * 60 * 5}); // locally cache authentication(s)
+const cache = new Map ();
 
-const defaultCert = generateCertificate ();
+// generate default key, certificate and sign it
+log.debug ('generating keypair...');
+const keypair = rsa.generateKeyPair({bits: 4096, e: 0x10001});
+const defaultKey = Buffer.from (forge.pki.privateKeyToPem (keypair.privateKey));
+const privateKey = keypair.privateKey;
+const publicKey = keypair.publicKey;
+log.debug ('keypair successfully generated');
+log.debug ('creating certificate...');
+const cert = forge.pki.createCertificate ();
+log.debug ('certificate successfully created');
+// configure the certificate
+cert.publicKey = publicKey;
+cert.validity.notBefore = new Date ()
+// set validity to 128 years;
+cert.validity.notAfter = new Date ();
+cert.validity.notAfter.setFullYear (cert.validity.notBefore.getFullYear() + 128);
+cert.setSubject ([{
+    name: 'commonName',
+    value: `${os.hostname ()}.invalid`
+}]);
+// sign the certificate
+log.debug ('signing certificate...');
+cert.sign (privateKey, forge.md.sha256.create ());
+log.debug ('certifiacte signed');
+const defaultCert = Buffer.from (forge.pki.certificateToPem (cert));
 
 module.exports = https.createServer ({
     SNICallback: async (domain, callback) => {
         // get latest cert
-        const queryResponse = await redis.get (`cert${process.env.AGASSI_ACME_PRODUCTION ? '' : '.staging'}:${domain}`);
-
-        if (queryResponse) {
-            // got cert
-            log.debug (`got certificate for ${domain} from redis`);
-            return callback (null, tls.createSecureContext ({
-                key: fs.readFileSync (process.env.AGASSI_DEFAULT_KEY_FILE),
-                cert: queryResponse
-            }));
-        } else {
-            // did not get cert, use default
-            log.warn (`no certificate found for ${domain}`);
-            return callback (null, false);
+        let certPath = `/agassi/certificates/${process.env.AGASSI_ACME_PRODUCTION ? 'production' : 'staging'}/${domain}`;
+        let authorizedCert = null;
+        // try the cache
+        let cachedCert = cache.get (certPath);
+        if (cachedCert) {
+            // found a cert in the cache
+            log.debug (`got cached cert for domain ${domain}`);
+            authorizedCert = cachedCert;
+        } else { // no cached cert
+            // try from etcd
+            authorizedCert = await etcdClient.get (certPath);
+            if (authorizedCert) { // got a cert fom etcd
+                // cache the cert from etcd
+                log.debug (`got cert for domain ${domain} from etcd, caching...`);
+                cache.set (certPath, authorizedCert);
+                log.dubg ('set cert in cache');
+                return callback (null, tls.createSecureContext ({
+                    key: defaultKey,
+                    cert: authorizedCert
+                }));
+            } else { // no cert from etcd or cache
+                log.warn (`no certificate found for ${domain}`);
+                return callback (null, tls.createSecureContext ({
+                    key: defaultKey,
+                    cert: defaultCert
+                }));
+            }
         }
     },
-    key: fs.readFileSync (process.env.AGASSI_DEFAULT_KEY_FILE),
+    key: defaultKey,
     cert: defaultCert
 }, async (request, response) => {
     const requestURL = new URL (request.url, `https://${request.headers.host}`);
-    const virtualHost = await redis.hgetall (`vhost:${requestURL.hostname}`);
+    const vHostPath = `/agassi/virtual-hosts/${requestURL.hostname}`;
+    let virtualHost = null;
+    // check cache for virtual host
+    virtualHost = cache.get (vHostPath);
+    if (!virtualHost) { // no vHost in cache 
+        // this will set virtualHost to null (again) if there is no vHost in etcd
+        virtualHost = await etcdClient.get (`/agassi/virtual-hosts/${requestURL.hostname}`);
+        if (virtualHost) { // got virtual host from etcd
+            log.debug (`got virtual host for domain from etcd`);
+            // parse the virtual host from etcd
+            virtualHost = JSON.parse (virtualHost)
+            log.debug ('cacheing virtual host...');
+            // set cache to parsed virtual host so that we don't parse it again
+            cache.set (vHostPath, virtualHost);
+            log.debug ('cache set');
+        }
+    }
+    // still don't have virtual host
     // if it doesn't have .options it doesn't have a target or forward
-    if (!virtualHost.options) {
+    if (!virtualHost || !virtualHost.options) {
         log.trace (`no target found for domain ${requestURL.hostname}`);
         response.writeHead(404, {
             'Content-Type': 'text/plain'
@@ -63,13 +115,13 @@ module.exports = https.createServer ({
 
     // parse proxy options
     // basic auth protected host
-    if (virtualHost.auth) {
-        log.trace ('auth required');
-        // auth required but not provided
+    if (virtualHost.authorization) {
+        log.trace ('authorization required');
+        // authorization required but not provided
         if (!request.headers.authorization) {
             // prompt for password in browser
             log.trace ('prompting password');
-            response.writeHead (401, { 'WWW-Authenticate': `Basic realm="Agassi"`});
+            response.writeHead (401, { 'WWW-Authenticate': `Basic realm="process.env.AGASSI_AUTH_REALM"`});
             response.end ('Authorization is required.');
             return;
         }
@@ -88,8 +140,8 @@ module.exports = https.createServer ({
         const requestAuth = (Buffer.from (request.headers.authorization.replace (/^Basic/, ''), 'base64')).toString ('utf-8');
         const [requestUser, requestPassword] = requestAuth.split (':');
 
-        // parse vHost auth parameter
-        const vHostAuth = (Buffer.from (virtualHost.auth, 'base64')).toString ('utf-8');
+        // parse vHost authorization parameter
+        const vHostAuth = (Buffer.from (virtualHost.authorization, 'base64')).toString ('utf-8');
         const [virtualUser, virtualHash] = vHostAuth.split (':');
 
 
@@ -102,7 +154,7 @@ module.exports = https.createServer ({
             log.trace ('authentication failed');
             rateLimit.inboundRequest (request);
             // prompt for password in browser
-            response.writeHead (401, { 'WWW-Authenticate': `Basic realm="Agassi"`});
+            response.writeHead (401, { 'WWW-Authenticate': `Basic realm="process.env.AGASSI_AUTH_REALM"`});
             response.end ('Authorization is required.');
         }
 
